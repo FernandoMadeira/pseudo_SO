c9380c95d148d7acf8738f5deb4f08de071fe025
Fernando Madeira <fernando.estatistico@gmail.com>
2020-11-13 22:16:48 -0300

    Create leitor_processos.py
    primeira versão do leitor de processos

---
+++ leitor_processos.py
@@ -0,0 +1,46 @@
+#leitor lista processos
+
+#endereco ='C:\Users\Fernando Windows\Documents'
+import os
+
+if os.getcwd() != os.path.join('C:\\','Users','Fernando Windows','Documents'):
+    os.chdir(os.path.join('C:\\','Users','Fernando Windows','Documents'))
+
+arquivo = 'processes.txt'
+
+
+lista_proc=open(arquivo)
+
+procs=lista_proc.readlines()
+#procs[1]
+
+tabela=[]
+
+for proc in procs:
+    dados={}
+    coluna=proc.split(', ')
+    dados['tempo_inic']=int(coluna[0])
+    dados['prior']=int(coluna[1])
+    dados['tempo_proc']=int(coluna[2])
+    dados['blocos']=int(coluna[3])
+    dados['cod_impr']=coluna[4]
+    dados['req_scan']=coluna[5]
+    dados['req_modem']=coluna[6]
+    dados['cod_disco']=coluna[7]
+    tabela=tabela+[dados]
+
+print(tabela)
+
+tempos_proc=[]
+
+for item in tabela:
+    tempos_proc=tempos_proc+[item['tempo_proc']]
+
+print(tempos_proc)
+
+
+
+import pprint
+
+pprint.pprint(tempos_proc)
+
