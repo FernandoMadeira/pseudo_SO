0abd610d70e9870563da4b34b11551c50a986a2d
Fernando Madeira <fernando.estatistico@gmail.com>
2020-12-04 22:34:52 -0300

    Update main.py
    arrumando uns endereços e alterando o nome dos modulos

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+import os
 import leitor_operacoes
 import leitor_processos
 
+
 def main():
     # endereco ='C:\Users\Fernando Windows\Documents'
-    endereco = '/Users/gabrieltomaz/Documents/UnB/1:2020/ISO/Trabalho/pseudo_SO'
+    endereco =os.path.join('C:\\','Users','Fernando Windows','Documents')
+    #endereco = '/Users/gabrieltomaz/Documents/UnB/1:2020/ISO/Trabalho/pseudo_SO'
     arquivo = 'files.txt'
     arquivo_processos = 'processes.txt'
 
     print(' ------------ Lendo Processos      ---------------- ')
-    leitor_processos.main(endereco, arquivo_processos)
+    leitor_processos(endereco, arquivo_processos)
     print(' ------------ Lendo Operacoes      ---------------- ')
-    leitor_operacoes.main(endereco, arquivo)
+    leitor_operacoes(endereco, arquivo)
 
 
 if __name__ == "__main__":
     main()
+
+
