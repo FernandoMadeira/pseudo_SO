6e1d165875a69bdbb8eb61b015a9b5c4d64ff5b2
Gabriel Tomaz <gabrieltomazlima@hotmail.com>
2020-12-05 15:08:18 -0300

    Atualizando a Main e Gerenciador de Memoria

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
-import os
 import leitor_operacoes
 import leitor_processos
+import gerenciador_memoria
 
 
 def main():
     # endereco ='C:\Users\Fernando Windows\Documents'
-    endereco =os.path.join('C:\\','Users','Fernando Windows','Documents')
-    #endereco = '/Users/gabrieltomaz/Documents/UnB/1:2020/ISO/Trabalho/pseudo_SO'
+    endereco = '/Users/gabrieltomaz/Documents/UnB/1:2020/ISO/Trabalho/pseudo_SO'
     arquivo = 'files.txt'
     arquivo_processos = 'processes.txt'
 
     print(' ------------ Lendo Processos      ---------------- ')
-    leitor_processos(endereco, arquivo_processos)
+    leitor_processos.main(endereco, arquivo_processos)
+
     print(' ------------ Lendo Operacoes      ---------------- ')
-    leitor_operacoes(endereco, arquivo)
+    operacoes = leitor_operacoes.main(endereco, arquivo)
+    print(operacoes)
+
+    print(' ------------ Inicializando Espaco de Memoria')
+    bloco_memoria = gerenciador_memoria.criar_espaco_memoria()
+    print(bloco_memoria)
+
+    print(' ------------ Operacoes na memoria --------- ')
+    gerenciador_memoria.processar_operacoes_memoria(operacoes)
+
+    print(' ------------ Blocos de memoria --------- ')
+    bloco_memoria = gerenciador_memoria.get_bloco_memoria_all()
+    print(bloco_memoria)
 
 
 if __name__ == "__main__":
     main()
-
-
