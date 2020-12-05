99d50e6635f3d6dd7437d66b8107b8664aa8e60e
Gabriel Tomaz <gabrieltomazlima@hotmail.com>
2020-12-04 21:10:15 -0300

    Refatorando leitor_operacoes

--- leitor_operacoes.py
+++ leitor_operacoes.py
@@ -1,52 +1,65 @@
 #leitor lista operacoes
 
-#endereco ='C:\Users\Fernando Windows\Documents'
 import os
 
-#endereco ='C:\Users\Fernando Windows\Documents'
-endereco ='/Users/gabrieltomaz/Documents/UnB/1:2020/ISO/Trabalho/pseudo_SO'
-if os.getcwd() != os.path.join(endereco):
-    os.chdir(os.path.join(endereco))
 
+def ler_arquivo(endereco, nmarquivo):
+
+    if os.getcwd() != os.path.join(endereco):
+        os.chdir(os.path.join(endereco))
+
+    linhasarq = open(nmarquivo)
+
+    linhas = linhasarq.readlines()
+
+    return linhas
+
+def ler_tabelas(linhas):
+
+    #procs[1]
+
+    blocos = int(linhas[0])
+
+    segmentos = int(linhas[1])
+
+    tabela=[]
+
+    for lin in linhas[2:(segmentos+2)]:
+        dados={}
+        coluna=lin.split(', ')
+        dados['arq']=coluna[0]
+        dados['bl_inicio']=int(coluna[1])
+        dados['bl_tam']=int(coluna[2])
+        tabela=tabela+[dados]
+
+    return tabela
+
+def ler_tabs(linhas):
+
+    tab = []
+
+    segmentos = int(linhas[1])
+
+    for lin in linhas[(segmentos+2):]:
+        dados = {}
+        coluna = lin.split(', ')
+        dados['proc'] = coluna[0]
+        dados['oper'] = coluna[1]
+        dados['arq'] = coluna[2]
+        if int(dados['oper']) == 0:
+            dados['blocos']=coluna[3]
+        tab=tab+[dados]
+    return tab
+
+
+#endereco ='C:\Users\Fernando Windows\Documents'
+endereco = '/Users/gabrieltomaz/Documents/UnB/1:2020/ISO/Trabalho/pseudo_SO'
 arquivo = 'files.txt'
 
-
-linhasarq = open(arquivo)
-
-linhas = linhasarq.readlines()
-#procs[1]
-
-blocos = int(linhas[0])
-
-segmentos = int(linhas[1])
-
-
-
-tabela=[]
-
-for lin in linhas[2:(segmentos+2)]:
-    dados={}
-    coluna=lin.split(', ')
-    dados['arq']=coluna[0]
-    dados['bl_inicio']=int(coluna[1])
-    dados['bl_tam']=int(coluna[2])
-    tabela=tabela+[dados]
+linhas = ler_arquivo(endereco, arquivo)
+tabela = ler_tabelas(linhas)
+tab = ler_tabs(linhas)
 
 print(tabela)
 
-tab=[]
-
-for lin in linhas[(segmentos+2):]:
-    dados={}
-    coluna=lin.split(', ')
-    dados['proc']=coluna[0]
-    dados['oper']=coluna[1]
-    dados['arq']=coluna[2]
-    if int(dados['oper'])==0:
-        dados['blocos']=coluna[3]
-    tab=tab+[dados]
-
 print(tab)
-
-
-
