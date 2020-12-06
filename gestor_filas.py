22bba4c65aeea503bc5595e0a3b5f271823b6704
Fernando Madeira <fernando.estatistico@gmail.com>
2020-12-06 11:11:19 -0300

    Update gestor_filas.py
    tirando parte que não aproveita

--- gestor_filas.py
+++ gestor_filas.py
@@ -1,46 +1,5 @@
 #gestor de filas
 
-#como saber que um processo é de usuário ou é de núcleo (tempo real)?
-
-
-#fila de usuario
-
-#entrada: dicionario chave-valor dos processos
-
-#saida: lista ordenada dos processos prontos
-
-#exemplo simples:
-#entrada: lista de processos e suas prioridades
-#saida: lista ordenada de processos
-
-#print('insira a quantidade de processos')
-#qtd=int(input())
-
-#pids=[5,4,8,2,3]
-#prioridade=[0,1,2,1,0]
-#tempo_ini=[3,2,1,4,5]
-#processos = {'n':nome,'p':prioridade}
-#processos.keys()
-#processos.values()
-
-#prioridade1 = prioridade.sort()
-
-
-#procs=[]
-#for i in range(len(pids)):
-#    procs.append({'pid':pids[i],'prioridade':prioridade[i],'tempo_inic':tempo_ini[i]})
-
-
-#procsord = sorted(procs, key='prioridade')
-#procsord = sorted(procs, key=lambda row:row['prioridade'])
-#procsfiltrados
-#0 in procs[1].values()
-#procsfiltrados = {k: v for k,v in procs.items()}
-#procsfiltrados = []
-
-#procs[2]['prioridade']
-
-
 #tempo real:
 def temporeal(procs):
     procstemporeal = []
@@ -60,7 +19,7 @@ def temporealt(procs,prioridade,tempo):
 def fifo(procs):
     procsord = sorted(procs, key=lambda row:row['tempo_inic'])
     return procsord 
-#procstemporealord = sorted(procstemporeal, key=lambda row:row['tempo_inic'])
+
 
 #usuario:
 def usuario(procs):
