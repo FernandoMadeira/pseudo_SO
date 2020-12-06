123e6879700cd78cc5a85b8190aae75cd44a4d59
Fernando Madeira <fernando.estatistico@gmail.com>
2020-12-06 16:50:47 -0300

    Update main.py
    ok

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main():
 
     print(' ---------------------- Blocos de memoria ---------------------- ')
     bloco_memoria = gerenciador_memoria.get_bloco_memoria_all()
-    print(bloco_memoria)
+    #print(bloco_memoria)
 
 
 if __name__ == "__main__":
