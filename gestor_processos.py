a7cccb1c6dbe69d37c0fb8cd3de6c7dd03c552ce
Fernando Madeira <fernando.estatistico@gmail.com>
2020-12-06 12:30:26 -0300

    Update gestor_processos.py
    executa processo

--- gestor_processos.py
+++ gestor_processos.py
@@ -12,7 +12,21 @@ def status_processo(processo,processos,tempo):
         return 'Ainda não entrou na fila'
 
 
-#def efetua_operacoes(proc,processos,t):
+def efetua_operacoes(proc,processos,t):
+    if tem_memoria(proc,processos,t):
+        if status_processo(processo,processos,tempo) == 'Em execução' and proc['tempo_proc']>0:
+            if primeira_execucao(proc,processos):   #criar essa função
+                if proc['oper']=='1':
+                    arq_removido = proc['arq']
+                    remover_arquivo(arq_removido)      #criar essa função dentro do sistema de arquivos
+                elif tab[j]['oper']=='0':
+                    arq_adicionado = tab[j]['arq']
+                    adicionar_arquivo(arq_adicionado)      #criar essa função dentro do sistema de arquivos
+                else:
+                    print('Operação desconhecida!')
+            else:
+                proc['tempo_proc'] = proc['tempo_proc'] - 1
+
 
 
 
