1e6c3df309828afebbc05f84457cbb64f844e2d9
Fernando Madeira <fernando.estatistico@gmail.com>
2020-12-06 16:49:58 -0300

    Update gestor_processos.py
    inicializa processos e executa

--- gestor_processos.py
+++ gestor_processos.py
@@ -1,6 +1,15 @@
 import gestor_filas
 import gerenciador_arquivo
 import gerenciador_memoria
+import leitor_operacoes
+
+def processos_inicializados(processos):
+    for proc in processos:
+        proc['exec'] = 0
+    return processos
+
+def primeira_execucao(proc):
+    proc['exec'] = 1
 
 
 def status_processo(processo,processos,tempo):
@@ -14,20 +23,27 @@ def status_processo(processo,processos,tempo):
         return 'Ainda não entrou na fila'
 
 
-def efetua_operacoes(proc,processos,t):
-    if tem_memoria(proc,processos,t):
+def efetua_operacoes(proc,processos,operacoes,t):
+    if leitor_operacoes.tem_memoria(proc,processos,t): #gerenciador_memoria.verifica_espaco_memoria_disponivel(64, False)
+        #ADICIONAR PROCESSO NA MEMÓRIA
+        processo_usuario == proc['prior'] in [1,2,3]
         if status_processo(processo,processos,tempo) == 'Em execução' and proc['tempo_proc']>0:
-            if primeira_execucao(proc,processos):   #criar essa função
-                if proc['oper']=='1':
-                    arq_removido = proc['arq']
-                    remover_arquivo(arq_removido)      #criar essa função dentro do sistema de arquivos
-                elif tab[j]['oper'] == '0':
-                    arq_adicionado = tab[j]['arq']
-                    adicionar_arquivo(arq_adicionado)      #criar essa função dentro do sistema de arquivos
-                else:
-                    print('Operação desconhecida!')
+            if proc['exec'] == 0: #primeira_execucao(proc,processos):   #criar essa função
+                primeira_execucao(proc)
+                for opr in operacoes:
+                    if opr['pid'] == proc['pid']:
+                        if opr['oper']=='1':
+                            arq_removido = opr['arq']
+                            #remover_arquivo(arq_removido)
+                            gerenciador_arquivo.remover_arquivo_disco(arq_removido, opr['pid'], processo_usuario)    #remover_arquivo_disco(arq, pid, processo_usuario)  #criar essa função dentro do sistema de arquivos
+                        elif opr['oper'] == '0':
+                            arq_adicionado = opr['arq']
+                            gerenciador_arquivo.adicionar_arquivo_disco(arq_adicionado,opr['pid'],opr['blocos'])      #criar essa função dentro do sistema de arquivos
+                        else:
+                            print('Operação desconhecida!')
             else:
                 proc['tempo_proc'] = proc['tempo_proc'] - 1
+        #REMOVER PROCESSO NA MEMÓRIA
 
 
 
