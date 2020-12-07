0209a890ef64a3973b61472ad6989deaa49266b7
Gabriel Tomaz <gabrieltomazlima@hotmail.com>
2020-12-07 17:46:28 -0300

    Atualizando README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# pseudo_SO
+# Pseudo_SO
  Códigos e arquivos para o trabalho de desenvolvimento de um pseudo SO.
+ 
+ 
+## Passos para execução
+    Utilize a versão do Python 3.7 ou superior 
+    Abra o arquivo main.py 
+    Na linha 7 altere a variavel 'endereco' para o caminho onde encontram-se os arquivos de processos e operações.
+    Execute a main.py   
+    
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import gestor_operacoes
 import leitor_processos
-import gerenciador_memoria
-import gerenciador_arquivo
-import gestor_filas
 import gestor_processos
 
 
@@ -15,24 +12,12 @@ def main():
     print(' ---------------- Lendo Processos ---------------- ')
     tabela_processos = leitor_processos.main(endereco, arquivo_processos)
 
-    # print('------------- Fila em cada tempo --------------------')
-    # for t in range(10):
-    #     print('tempo '+str(t))
-    #     print(gestor_filas.filatempo(tabela_processos,t))
-    #
-    # print('------------- Fila em cada tempo PID --------------------')
-    # for t in range(10):
-    #     print('tempo '+str(t))
-    #     print(leitor_processos.lista_atributo(gestor_filas.filatempo(tabela_processos,t),'pid'))
-
-    # print(' ---------------------- Lendo Operacoes ---------------------- ')
     gestor_operacoes.main(endereco, arquivo)
     linhas = gestor_operacoes.ler_arquivo(endereco, arquivo)
     operacoes = gestor_operacoes.ler_operacoes_processo(linhas)
 
     print(' ---------------- Dispatcher      ---------------- ')
     gestor_processos.dispatcher(tabela_processos, operacoes)
-    # exit()
 
     print(' ---------------- Execução ---------------- ')
     print('Sistema de arquivos =>')
