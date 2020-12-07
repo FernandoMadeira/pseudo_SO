f75be8f03b81f4a034d40afc242769bb4f453a9a
Fernando Madeira <fernando.estatistico@gmail.com>
2020-12-06 21:31:04 -0300

    Update gerenciador_memoria.py
    troca do 0 por []

--- gerenciador_memoria.py
+++ gerenciador_memoria.py
@@ -6,7 +6,7 @@ memoria = []
 def criar_espaco_memoria():
     print(' ---------- CRIANDO ESPACO DE MEMORIA! ---------- ')
     for i in range(1024):
-        memoria.append(0)
+        memoria.append([])
     print(' ---------- ESPACO DE MEMORIA CRIADO COM SUCESSO! ---------- ')
 
     return True
@@ -17,7 +17,7 @@ def remover_processo_memoria(pid):
 
     for posicao in range(len(memoria)):
         if memoria[posicao] == pid:
-            memoria[posicao] = 0
+            memoria[posicao] = []
 
 
 # Funcao responsável por retornar o estado atual da memoria
@@ -39,11 +39,11 @@ def verifica_espaco_memoria_disponivel(tam_bloco, processo_usuario):
     if processo_usuario:
         # remove os 64 primeiros blocos que estao reservados para processos em tempo real
         memoria_usuario = memoria[64:]
-        blocos = [0]*tam_bloco
+        blocos = [[]]*tam_bloco
         blocos_disp = blocos in [memoria_usuario[i:i + tam_bloco] for i in range(len(memoria_usuario) - 1)]
 
     else:
-        blocos = [0] * tam_bloco
+        blocos = [[]] * tam_bloco
         blocos_disp = blocos in [memoria[i:i + tam_bloco] for i in range(len(memoria) - 1)]
 
     return blocos_disp
@@ -54,17 +54,17 @@ def get_espaco_memoria_disponivel(tam_bloco, processo_usuario):
 
     espaco_disp = 0
     posicoes = []
+    range_fim = 1024
 
     if processo_usuario:
-        range_ini = 65
-        range_fim = 1025
+        range_ini = 64
     else:
         range_ini = 0
-        range_fim = 1025
+
 
     for posicao in range(range_ini, range_fim):
 
-        if memoria[posicao] == 0 and espaco_disp != tam_bloco:
+        if memoria[posicao] == [] and espaco_disp < tam_bloco:
             espaco_disp = espaco_disp+1
             posicoes.append(posicao)
 
@@ -87,7 +87,7 @@ def adicionar_processo_memoria(proc,processos,t):
     if verifica_espaco_memoria_disponivel(proc['blocos'], processo_usuario):
         posicoes_mem = get_espaco_memoria_disponivel(proc['blocos'], processo_usuario)
         if posicoes_mem != 0:
-            escrever_bloco_memoria_em_lote(posicoes_mem, '1')
+            escrever_bloco_memoria_em_lote(posicoes_mem, proc['pid'])
             return True
         else:
             print('Erro! Memoria insuficiente!')
